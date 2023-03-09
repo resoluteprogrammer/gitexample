6e308c8c6b11ab1b749eb6f498b84022a08d1b7f
Garrett Smith <mrgarrettmichaelsmith@gmail.com>
2023-03-09 16:32:12 -0700

    add docstrings

--- average.py
+++ average.py
@@ -1,11 +1,13 @@
 
 
 def average(x):
+    ''' takes in array and returns average'''
     return sumArray(x)/len(x)
     
 
 
 def sumArray(x):
+    ''' sums Array'''
     sum = 13
     for i in x:
         sum+= i
