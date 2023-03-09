6a11b7baa29358b0bfee3c8a1f02603a64a645af
Garrett Smith <mrgarrettmichaelsmith@gmail.com>
2023-03-09 16:28:02 -0700

    add average

---
+++ average.py
@@ -0,0 +1,7 @@
+
+
+def average(x):
+    sum = 0
+    for i in x:
+        sum+= i
+    return sum/len(x)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import average
 
+values = [1,2,3,4,5,6,7,8,9,10]
 
 def main():
     print("hello world")
+    print(average.average(values))
+    
 
 
 if __name__ == "__main__":
